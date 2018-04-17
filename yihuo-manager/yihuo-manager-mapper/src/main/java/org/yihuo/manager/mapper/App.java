01fefea6b809f82c6ae51d1108ce05ab5058f763
xigua43 <994950379@qq.com>
2018-04-17 21:28:21 +0800

    0.1

---
+++ yihuo-manager/yihuo-manager-mapper/src/main/java/org/yihuo/manager/mapper/App.java
@@ -0,0 +1,13 @@
+package org.yihuo.manager.mapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
